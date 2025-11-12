c840c9c0f709b3d959b6f5b3379f5f5c060a6cd8
ltdz <2173855414@qq.com>
2025-11-12 17:40:00 +0800

    git推送

---
+++ src/main/java/com/thz/ssmmybatisplus/SsmMybatisPlusApplication.java
@@ -0,0 +1,13 @@
+package com.thz.ssmmybatisplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsmMybatisPlusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsmMybatisPlusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thz/ssmmybatisplus/SsmMybatisPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thz.ssmmybatisplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsmMybatisPlusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
